5bf2de3113a0e5cfea0777a0e301217f4861b88d
Ka Mil <kjezier@gmail.com>
2018-07-15 21:09:31 +0200

    Added licence coment

--- Engine/Star.h
+++ Engine/Star.h
@@ -1,5 +1,25 @@
 #pragma once
 
+/******************************************************************************************
+*	Chili DirectX Framework Version 16.07.20											  *
+*	Star.h																	  *
+*	Copyright 2016 PlanetChili <http://www.planetchili.net>								  *
+*																						  *
+*	This file is part of The Chili DirectX Framework.									  *
+*																						  *
+*	The Chili DirectX Framework is free software: you can redistribute it and/or modify	  *
+*	it under the terms of the GNU General Public License as published by				  *
+*	the Free Software Foundation, either version 3 of the License, or					  *
+*	(at your option) any later version.													  *
+*																						  *
+*	The Chili DirectX Framework is distributed in the hope that it will be useful,		  *
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of						  *
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  *
+*	GNU General Public License for more details.										  *
+*																						  *
+*	You should have received a copy of the GNU General Public License					  *
+*	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
+******************************************************************************************/
 
 #include <vector>
 #include "JC_Vector2.h"

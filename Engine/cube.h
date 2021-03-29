d262d3ada295d134d732113496e1ae4944f6ca46
veeru <immaculateempyrean@gmail.com>
2021-03-29 10:35:47 +0530

    created a Uv cube to be displayed as an example that texture mapping is working as intended.

--- Engine/cube.h
+++ Engine/cube.h
@@ -9,70 +9,82 @@ public:
 	{
 		texturedVertex vertex;
 
-		vertex.m_position = Vec3(-side, -side, -side);
-		vertex.m_uv_coordinates = Vec2(0.0f, 1.0f);
+		vertex.m_position = Vec3(side, side, side);
+		vertex.m_uv_coordinates = Vec2(0.0f, 0.5f);
 		vertexBuffer.emplace_back(vertex); //0
 
-		vertex.m_position = Vec3(side, -side, -side);
-		vertex.m_uv_coordinates = Vec2(1.0f, 1.0f);
+		vertex.m_position = Vec3(-side, side, side);
+		vertex.m_uv_coordinates = Vec2(0.25f, 0.5f);
 		vertexBuffer.emplace_back(vertex); //1
 		
 		vertex.m_position = Vec3(-side, side, -side);
-		vertex.m_uv_coordinates = Vec2(0.0f, 0.0f);
+		vertex.m_uv_coordinates = Vec2(0.5f, 0.75f);
 		vertexBuffer.emplace_back(vertex); //2
 
 		vertex.m_position = Vec3(side, side, -side);
-		vertex.m_uv_coordinates = Vec2(1.0f, 0.0f);
+		vertex.m_uv_coordinates = Vec2(0.0f, 0.75f);
 		vertexBuffer.emplace_back(vertex); //3
 		
 		vertex.m_position = Vec3(-side, -side, side);
-		vertex.m_uv_coordinates = Vec2(1.0f, 1.0f);
+		vertex.m_uv_coordinates = Vec2(0.5f, 0.5f);
 		vertexBuffer.emplace_back(vertex); //4
 		
-		vertex.m_position = Vec3(side, -side, side);
-		vertex.m_uv_coordinates = Vec2(0.0f, 1.0f);
+		vertex.m_position = Vec3(-side, -side, -side);
+		vertex.m_uv_coordinates = Vec2(0.5f, 0.75f);
 		vertexBuffer.emplace_back(vertex); //5
 		
 		vertex.m_position = Vec3(-side, side, side);
-		vertex.m_uv_coordinates = Vec2(1.0f, 0.0f);
+		vertex.m_uv_coordinates = Vec2(0.5f, 0.25f);
 		vertexBuffer.emplace_back(vertex); //6
 		
 		vertex.m_position = Vec3(side, side, side);
-		vertex.m_uv_coordinates = Vec2(0.0f, 0.0f);
+		vertex.m_uv_coordinates = Vec2(0.75f, 0.25f);
 		vertexBuffer.emplace_back(vertex); //7
 
+		vertex.m_position = Vec3(side, -side, side);
+		vertex.m_uv_coordinates = Vec2(0.75f, 0.5f);
+		vertexBuffer.emplace_back(vertex); //8
+		
+		vertex.m_position = Vec3(side, -side, -side);
+		vertex.m_uv_coordinates = Vec2(0.75f, 0.75f);
+		vertexBuffer.emplace_back(vertex); //9
 
-		setLineIndexBuffer();
+		vertex.m_position = Vec3(side, side, -side);
+		vertex.m_uv_coordinates = Vec2(0.75f, 1.0f);
+		vertexBuffer.emplace_back(vertex); //10
+
+		vertex.m_position = Vec3(-side, side, -side);
+		vertex.m_uv_coordinates = Vec2(0.5f, 1.0f);
+		vertexBuffer.emplace_back(vertex); //11
+
+		vertex.m_position = Vec3(side, side, -side);
+		vertex.m_uv_coordinates = Vec2(1.0f, 0.75f);
+		vertexBuffer.emplace_back(vertex); //12
+
+		vertex.m_position = Vec3(side, side, side);
+		vertex.m_uv_coordinates = Vec2(1.0f, 0.5f);
+		vertexBuffer.emplace_back(vertex); //13
+
 		setTriangleIndexBuffer();
 	}
 
-	inline std::vector<uint32_t> getLineIndexBuffer		() { return lineIndexBuffer;	 }
-	inline std::vector<uint32_t> getTriangleIndexBuffer	() { return triangleIndexBuffer; }
-
-	inline std::vector<texturedVertex>&     getVertexBuffer() { return vertexBuffer; }
+	inline std::vector<uint32_t> getIndexBuffer	() { return m_indexBuffer; }
+	inline std::vector<texturedVertex>&     getVertexBuffer() { return m_vertexBuffer; }
 
 private:
-	void setLineIndexBuffer()
-	{
-		lineIndexBuffer = std::vector<std::uint32_t>{
-			0,1,  1,3,  3,2,  2,0,
-			0,4,  1,5,	3,7,  2,6,
-			4,5,  5,7,	7,6,  6,4 };
-	}
+	
 	void setTriangleIndexBuffer()
 	{
-		triangleIndexBuffer = std::vector<std::uint32_t>{
-				0, 2, 1, 2, 3, 1,
-				1, 3, 5, 3, 7, 5,
-				2, 6, 3, 3, 6, 7,
-				4, 5, 7, 4, 7, 6,
-				0, 4, 2, 2, 4, 6,
-				0, 1, 4, 1, 5, 4 };
+		m_indexBuffer = std::vector<std::uint32_t>{
+				1,0,3	, 3,2,1,
+				4,1,2	, 2,5,4,
+				8,4,5	, 5,9,8,
+				11,10,9 , 9,5,11,
+				12,13,8 , 8,9,12
+		};
 	}
 
 private:
-	std::vector<texturedVertex> vertexBuffer;
-
-	std::vector<std::uint32_t> lineIndexBuffer;
-	std::vector<uint32_t> triangleIndexBuffer;
+	std::vector<texturedVertex> m_vertexBuffer;
+	std::vector<uint32_t> m_indexBuffer;
 };

99a2c40a16682b55ef89b7979d5bf0d1ad329cd4
саня <sanyax64@yandex.ru>
2022-07-22 18:01:50 -0300

    add rectl for integers

---
+++ Engine/RectI.h
@@ -0,0 +1,21 @@
+#include "Vei2.h"
+
+class RectI
+{
+public:
+	RectI() = default;
+	RectI(int left_in, int right_in, int top_in, int bottom_in);
+	RectI(const Vei2& topLeft, const Vei2& bottomRight);
+	RectI(const Vei2& topLeft, int width, int height);
+	bool IsOverlappingWith(const RectI& other) const;
+	bool IsContainedBy(const RectI& other) const;
+	bool Contains(const Vei2& point) const;
+	static RectI FromCenter(const Vei2& center, int halfWidth, int halfHeight);
+	RectI GetExpanded(int offset) const;
+	Vei2 GetCenter() const;
+public:
+	int left;
+	int right;
+	int top;
+	int bottom;
+};
